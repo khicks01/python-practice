0ba10de6f6520ee9f3e09ac3a6908dff351ecb9d
Kyle Hicks <kylekhicks@gmail.com>
2020-01-18 16:18:54 -0600

    added myFirstFunction

--- practice.py
+++ practice.py
@@ -3,3 +3,7 @@ input("Please enter the string you want to be printed out: ")
 
 #Saving the input to a variable
 user_says = input("Please enter the string you want to be printed out: ")
+
+def myFirstFunction():
+    print("This is my first function")
+myFirstFunction()
