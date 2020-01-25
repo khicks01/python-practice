f575100a0a3df88e72b63619c81eb9b97757e511
Kyle Hicks <kylekhicks@gmail.com>
2020-01-25 10:35:23 -0600

    starting calc

--- practice.py
+++
@@ -1,9 +0,0 @@
-#User input
-input("Please enter the string you want to be printed out: ")
-
-#Saving the input to a variable
-user_says = input("Please enter the string you want to be printed out: ")
-
-def myFirstFunction():
-    print("This is my first function")
-myFirstFunction()
