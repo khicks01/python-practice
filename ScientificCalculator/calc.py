aa0c1103af1b5c1ec6478f66ceb6cc5a0ccb9dcf
Kyle Hicks <kylekhicks@gmail.com>
2020-01-25 11:11:11 -0600

    added sum

--- ScientificCalculator/calc.py
+++ ScientificCalculator/calc.py
@@ -3,6 +3,18 @@ operationList = ["Addition", "Subtraction", "Multiplication",
 "Division", "Modulo", "Raising to a power", "Square root", "Logarithm", "Sine",
 "Cosine", "Tangent"]
 
+def twoNumberChooser():
+    x = input("Choose the first number: ")
+    print()
+    y = input("Choose the second number: ")
+    returnTuple = (x,y)
+    return returnTuple
+ 
+def sum_x_y(numberTuple):
+    num1 = int(numberTuple(0))
+    num2 = int(numberTuple(1))
+    return (num1+num2)
+
 while looper:
     print("Choose the math operation or type EXIT:")
     print()
@@ -10,6 +22,11 @@ while looper:
         print(str(operationList.index(element)) +" - "+str(element))
     print()
     userChoice = input()
+    print()
     if userChoice.upper() =="EXIT":
         looper = False
+    elif (userChoice)=="0":
+        numbers = twoNumberChooser()
+        print(str(sum_x_y(numbers)))
+
 
