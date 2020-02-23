8add70a7d468667d7414a22e77a279ff0bd80ea6
Kyle Hicks <kylekhicks@gmail.com>
2020-02-23 12:13:49 -0600

    added pycharm

---
+++ Mallika/helloworld.py
@@ -0,0 +1,8 @@
+# this file will print Hello world to the screen
+
+# create the message
+message = "hello world"
+
+# print the message out to the screen
+
+print(message)
---
+++ Mallika/practice.py
@@ -0,0 +1,2 @@
+helloworld = "hello"
+print(helloworld)
